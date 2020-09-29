ef0433db6910b864b1b6618d4e2ef96874a8456a
Jack Daoud <35039897+JackDaoud@users.noreply.github.com>
2020-09-29 13:47:34 -0400

    Update

--- Python/02 Basic_Data_Types/01 List Comprehensions.py
+++ Python/02 Basic_Data_Types/01 List Comprehensions.py
@@ -8,3 +8,4 @@ if __name__ == '__main__':
     n = int(input())
 
 # Written Code:
+print([[i, j, k] for i in range(x + 1) for j in range(y + 1) for k in range(z + 1) if i + j + k != n])
---
+++ Python/02 Basic_Data_Types/02 Find the Runner-Up Score!.py
@@ -0,0 +1,8 @@
+# Problem: https://www.hackerrank.com/challenges/find-second-maximum-number-in-a-list/problem
+
+# Given Code:
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+
+# Written Code:
